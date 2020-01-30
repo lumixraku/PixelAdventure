b100cc52bfc462d4b5b701fc5cb57d7d9df0e336
luonan <luonan@bytedance.com>
2020-01-30 20:50:24 +0800

    first commit!
    Change-Id: Ida8e2efffeb6d77964febeeb3214fd3e90c2b7d9

---
+++ Assets/Scripts/animateBG.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateBG : MonoBehaviour
+{
+    public Vector2 moveSpeed;
+    public Vector2 offset;
+    private Material material;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        material = GetComponent<Renderer>().material ;
+        //material.SetTextureOffset("brownBG", moveSpeed);
+        material.mainTextureOffset = moveSpeed;
+    }   
+         
+    // Update is called once per frame
+    void Update()
+    {
+        offset = material.mainTextureOffset;
+        material.mainTextureOffset += moveSpeed * Time.deltaTime;
+        // Debug.LogFormat("delta time {0}", Time.deltaTime); Time.deltaTime 是一帧所经历的时间
+    }
+}
